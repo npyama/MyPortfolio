4406c1d383a18dcdc6f3a945375bd4f3eb810e9a
npyama <drowning.now@gmail.com>
2012-11-17 10:21:22 -0800

    add flick to bg

--- Develop/scripts/main.js
+++ Develop/scripts/main.js
@@ -1,4 +1,31 @@
 $(function(){
+
+
+
+//URLバー隠し
+setTimeout(function(){  window.scrollTo(0,1);},1000); 
+$(function(){
+	window.scrollTo(0,1);
+});
+
+
+//フリック
+(function () {
+	Flipsnap('#bg .flipsnap');
+})(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
 	$("#nav_prev_arrow").hover(function() {
 		$("#nav_box").animate(
 		{ 
@@ -26,34 +53,11 @@ $("li").toggle(
     $(this).css({"list-style-type":"", "color":""});
   });
 
-
-
-
-//1000ミリ秒の遅延を設定しているのはページの読込が完了するまでの十分な時間として設定してます。
-setTimeout(function(){  window.scrollTo(0,1);},1000);
- 
-//jQueryなどだとページ読込み完了時に呼んでやるのもありです。
-$(function(){
-window.scrollTo(0,1);
-});
-
-
-
 	
 });
 
 
 
 
-$(function(){
-	var $container = $('.container','#flick');
-	var $item = $('.item','#flick');
-	$(window).bind(('orientationchange resize load'), function(){
-		var windowWidth = (!(window.innerWidth)) ? document.documentElement.clientWidth : window.innerWidth;
-		
-		$item.css({width : windowWidth});
-		$container.css({width : windowWidth})
-	});
 
-	$("#flick").flick({});
-});
+
