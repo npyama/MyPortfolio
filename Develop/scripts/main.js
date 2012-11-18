886ffc3b5b00b6c564b0458e242dd68377377037
npyama <drowning.now@gmail.com>
2012-11-17 18:41:14 -0800

    add easing

--- Develop/scripts/main.js
+++ Develop/scripts/main.js
@@ -1,32 +1,19 @@
 $(function(){
 
 
-//URLバー隠し
+/****************************************
+	URLバー隠し
+****************************************/
 setTimeout(function(){  window.scrollTo(0,1);},1000); 
 $(function(){
 	window.scrollTo(0,1);
 });
 
 
-//フリック
-/*
-(function () {
-	Flipsnap('#bg .flipsnap');
-})(); 
-*/
 
-/*
-var $pointer = $('#explain div');
-var flipsnap = Flipsnap('.flipsnap');
-
-flipsnap.element.addEventListener('fsmoveend', function() {
-    $pointer.filter('.current').removeClass('current');
-    $pointer.eq(flipsnap.currentPoint).addClass('current');
-    $("#explain div").css("opacity", "0");
-    $("#explain .current").css("opacity", "1");
-}, false);
-*/
-
+/****************************************
+	フリック
+****************************************/
 var $pointer = $('#explain div');
 var flipsnap = Flipsnap('.flipsnap');
 
@@ -38,7 +25,10 @@ flipsnap.element.addEventListener('fsmoveend', function() {
 }, false);
 
 
-//nav_thumb リンク
+
+/****************************************
+	nav_thumb リンク
+****************************************/
 $('#nav_thumb #no01').click(function() {
     flipsnap.moveToPoint(0);
 });
@@ -51,7 +41,11 @@ $('#nav_thumb #no03').click(function() {
     flipsnap.moveToPoint(2);
 });
 
-//カスタムスクロールバー
+
+
+/****************************************
+	カスタムスクロールバー
+****************************************/
 (function($){
     $(window).load(function(){
       $("#nav_thumb").mCustomScrollbar();
@@ -69,26 +63,10 @@ $('#nav_thumb #no03').click(function() {
 
 
 
-
-
-//nav,explain表示
-/*	
-	$("#bg").onclick(function() {
-	$(this).css("display", "dis")
-			.css("color", "blue");
-	});
-	
-	
-	$("#nav_next_arrow").hover(function() {
-		$("#nav_box").animate(
-		{ 
-		paddingTop: "0px",
-		paddingBottom: "100px",
-		}
-		, 1500 );
-	});
-*/
-
+/****************************************
+	nav,explain表示
+****************************************/
+/*
 $("#bg").toggle(function () {
 	$("nav").css("display", "block")
 	},function () {
@@ -101,33 +79,21 @@ $("#bg").toggle(function () {
 	},function () {
 	$("#explain").css("display", "none");
 });
-
-/*
-
-$("#bg #no01").toggle(function () {
-	$("#explain #no01").css("display", "block")
-	},function () {
-	$("#explain #no01").css("display", "none");
-});
-
-
-$("#bg #no02").toggle(function () {
-	$("#explain #no02").css("display", "block")
-	},function () {
-	$("#explain #no02").css("display", "none");
-});
-
-
-$("#bg #no03").toggle(function () {
-	$("#explain #no03").css("display", "block")
-	},function () {
-	$("#explain #no03").css("display", "none");
-});
-
 */
 
 
-	
+
+
+
+$("#bg").click(function(){
+	$("nav").animate({ 
+	left: "0%"
+	}, { duration:400, easing: 'easeInOutQuint', });
+});
+
+
+
+
 });
 
 
